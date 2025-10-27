286ea7877f3692db081379a53c42164247cd21a9
miaan15 <miaanstarman@gmail.com>
2025-10-28 03:38:32 +0700

    update benchmark

--- bench/main.cpp
+++ bench/main.cpp
@@ -11,4 +11,32 @@ static void BM_SparseSetInsert(benchmark::State& state) {
 }
 BENCHMARK(BM_SparseSetInsert)->Range(8, 8<<10);
 
+static void BM_SparseSetContains(benchmark::State& state) {
+  sparse_set<int> set;
+  for (int i = 0; i < state.range(0); ++i) {
+    set.insert(i);
+  }
+  for (auto _ : state) {
+    for (int i = 0; i < state.range(0); ++i) {
+      benchmark::DoNotOptimize(set.contains(i));
+    }
+  }
+}
+BENCHMARK(BM_SparseSetContains)->Range(8, 8<<10);
+
+static void BM_SparseSetErase(benchmark::State& state) {
+  sparse_set<int> set;
+  for (auto _ : state) {
+    state.PauseTiming();
+    for (int i = 0; i < state.range(0); ++i) {
+      set.insert(i);
+    }
+    state.ResumeTiming();
+    for (int i = 0; i < state.range(0); ++i) {
+      set.erase(i);
+    }
+  }
+}
+BENCHMARK(BM_SparseSetErase)->Range(8, 8<<10);
+
 BENCHMARK_MAIN();
